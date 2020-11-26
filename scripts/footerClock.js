f980bfc5ee6beb0d2b1371c0217a78512881b7c1
kouheyHEY <vudtarm@gmail.com>
2020-11-27 00:46:42 +0900

    make index.html

---
+++ scripts/footerClock.js
@@ -0,0 +1,31 @@
+var footerClock = function () {
+    var dateObj = new Date();
+    var year = dateObj.getFullYear();
+    var month = dateObj.getMonth();
+    var day = dateObj.getDate();
+    var hour = dateObj.getHours();
+    var minutes = dateObj.getMinutes();
+    var displayElement = document.getElementById('footerClock');
+    var timeStr = '';
+
+
+    if (hour < 10) {
+        hour = '0' + hour;
+    }
+
+    if (minutes < 10) {
+        minute = '0' + minutes;
+    }
+
+
+    timeStr = year + '/' + month + '/' + day + ' ';
+    timeStr += hour + ':' + minutes;
+
+    if (displayElement) {
+        displayElement.innerHTML = timeStr;
+    }
+
+    setTimeout(footerClock, 1000)
+};
+
+footerClock();
