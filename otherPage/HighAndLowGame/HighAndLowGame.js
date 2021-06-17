2a8b3076bb2e926b110b8214da345d0411c13b6e
kouheyHEY <vudtarm@gmail.com>
2021-06-18 02:08:37 +0900

    update gameOverand refund

--- otherPage/HighAndLowGame/HighAndLowGame.js
+++ otherPage/HighAndLowGame/HighAndLowGame.js
@@ -1,19 +1,19 @@
 // 各変数の設定
 currentChoice = 0;
-choice_no = 0;
-choice_high = 1;
-choice_low = 2;
-choice_equal = 3;
-choice_string = [
+const choice_no = 0;
+const choice_high = 1;
+const choice_low = 2;
+const choice_equal = 3;
+const choice_string = [
     "　　",
     "ハイ",
     "ロー",
     "イコール"
 ];
 
-gameResult_win = 1;
-gameResult_lose = 2;
-gameResult_equal = 3;
+const gameResult_win = 1;
+const gameResult_lose = 2;
+const gameResult_equal = 3;
 
 currentChain = 0;
 currentWin = 0;
@@ -25,15 +25,17 @@ newNumber = "";
 
 betCoinNum = 0;
 betCoinMin = 1;
-betCoinMax = currentCoin;
+betCoinMax = 100;
+const canRefundWinNum = 3;
 
 cardPool = [];
-cardNum = 4;
-MaxNumber = 10;
+const cardNum = 4;
+const MaxNumber = 10;
 currentNumber = -1;
 
 battleResult = 0;
 gameOverFlg = false;
+restartGameFlg = false;
 
 // 変数の初期化を行う
 initAll();
@@ -59,16 +61,22 @@ function initAll() {
     currentBattleNum = 0;
     currentRatio = 1;
     currentCoin = 100;
+    currentNumber = -1;
+    betCoinNum = 0;
+    battleResult = 0;
     gameOverFlg = false;
 
     // カードプールの初期化
     initcardPool();
 
-    // 各値の更新を行う。
+    // 各値の更新を行う
+    setValue("currentNumber", "");
     setValue("currentChain", currentChain + "連勝");
     setValue("currentWin", currentWin + "勝");
     setValue("currentBattleNum", currentBattleNum + "回");
     setValue("currentCoin", currentCoin + "枚");
+    setChoice(choice_no);
+
 }
 
 /**
@@ -92,16 +100,23 @@ function setValue(id, value) {
 
 /**
  * ハイかローを選択する
- * @param choiceNum 1, 2, 3 
+ * @param choiceNum 1:ハイ 2:ロー, 3:イコール 
  */
 function setChoice(choiceNum) {
+
+    if (showGameOver() == 0) {
+        // ゲームオーバーの場合、何もせず終了する
+        return;
+    }
+
     currentChoice = choiceNum;
     setValue("choice", choice_string[choiceNum]);
 }
 
 /**
  * カードをめくる
- * @returns [0-9]:正常 -1:残りカード0枚
+ * @returns [0-9] 正常
+ * @returns -1 残りカード0枚
  */
 function setNewCard() {
     settableNumber = [];
@@ -129,6 +144,12 @@ function setNewCard() {
  */
 
 function betCoin() {
+
+    if (showGameOver() == 0) {
+        // ゲームオーバーの場合、何もせず終了する
+        return;
+    }
+
     // 入力ダイアログを表示、賭けコイン枚数を入力
     betNum = window.prompt("賭けるコインの枚数を入力してください", "");
     if (isNaN(betNum)) {
@@ -137,6 +158,7 @@ function betCoin() {
         return;
     }
     betNum = Number(betNum);
+    betCoinMax = currentCoin;
     if (betNum < betCoinMin || betNum > betCoinMax) {
         // 入力範囲エラー
         window.alert("コインの枚数は(" + betCoinMin + "-" + betCoinMax + ")で入力してください");
@@ -189,6 +211,11 @@ function updateBetCoin(gameResult) {
         betCoinNum = 0;
         currentRatio = 1;
         currentChain = 0;
+
+        if (currentCoin == 0) {
+            // 持ちコインの枚数が0枚の場合、ゲームオーバーとする
+            gameOverFlg = true;
+        }
     }
     return;
 }
@@ -203,13 +230,23 @@ function setFirstCard() {
 
 /**
  * 勝負を行う
- * @returns 1: 勝ち 2: 負け 3: 引き分け
+ * @returns 1 勝ち
+ * @returns 2 負け
+ * @returns 3 引き分け
  */
 function runGame() {
+    if (gameOverFlg) {
+        window.alert("ゲームを再開します");
+        gameOverFlg = false;
+        restartGameFlg = true;
+        initAll();
+        return;
+    }
+
     if (currentNumber == -1) {
         // 最初のカードをセットする
         setFirstCard();
-        setValue("currentNumber", currentNumber);
+        setValue("currentNumber", currentNumber + 1);
         return;
     }
     // 賭けコインの枚数が足りていない場合
@@ -243,6 +280,13 @@ function runGame() {
  */
 
 function showResult() {
+    if (restartGameFlg) {
+        // ゲームの再開時は値の更新だけ行う
+        setValue("ratio", betCoinNum + "(×" + Math.floor(currentRatio) + ")");
+        restartGameFlg = false;
+        return;
+    }
+
     if (battleResult == gameResult_equal) {
         // 引き分けだった場合
         updateBetCoin(gameResult_equal);
@@ -265,4 +309,56 @@ function showResult() {
     setValue("currentCoin", currentCoin + "枚");
     setValue("currentWin", currentWin + "勝");
 
+}
+
+/**
+ * コインの払い戻しを行う
+ */
+
+function refund() {
+    if (showGameOver() == 0) {
+        // ゲームオーバーの場合、何もせず終了する
+        return;
+    }
+
+    if (currentChain < canRefundWinNum) {
+        // 連勝数が規定回数未満の場合
+        window.alert("連勝数が" + canRefundWinNum + "回未満です。");
+        return;
+    }
+
+    // 賭けコインを持ちコインに還元する
+    currentCoin += betCoinNum;
+    betCoinNum = 0;
+
+    // 連勝数をリセットする
+    currentChain = 0;
+    currentRatio = 0;
+
+    // カードプールと現在のカードをリセットする
+    initcardPool();
+    currentNumber = -1;
+    setValue("currentNumber", "");
+
+    // 各値の更新を行う
+    setChoice(choice_no);
+    setValue("ratio", betCoinNum + "(×" + currentRatio + ")");
+    setValue("currentChain", currentChain + "回");
+    setValue("currentCoin", currentCoin + "枚");
+
+}
+
+/**
+ * コインがない場合のアラートを表示する。
+ * @returns 0 ゲームオーバー
+ * @returns 1 ゲーム継続中
+ */
+
+function showGameOver() {
+    if (gameOverFlg) {
+        window.alert("ゲームオーバーです 再開する場合はカードをめくってください");
+        return 0;
+    } else {
+        return 1;
+    }
 }
