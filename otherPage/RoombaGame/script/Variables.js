a9765ed063b4a289467019b45814ee56dda53211
kouheyHEY <vudtarm@gmail.com>
2021-08-24 21:39:50 +0900

    update field

--- otherPage/RoombaGame/script/Variables.js
+++ otherPage/RoombaGame/script/Variables.js
@@ -1,9 +1,12 @@
-let FIELD_COL = 5;
-let FIELD_ROW = 5;
+const FIELD_COL = 5;
+const FIELD_ROW = 5;
 
 const STATE_ROOMBA = -1;
 const STATE_BLANK = 0;
 const STATE_TRASH = 1;
 const STATE_BLOCK = 2;
-const STATE_GOAL = 3;
+const STATE_BOMB = 3;
+const STATE_GOAL = 4;
 
+let field_masking = [];
+let field_true = [];
