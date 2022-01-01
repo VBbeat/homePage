13000b5a7a8f6723c634a422b9b9d00f14b1651f
kouheyHEY <vudtarm@gmail.com>
2022-01-01 14:41:25 +0900

    updateMasterEditor

--- php/masterEdit.php
+++ php/masterEdit.php
@@ -43,7 +43,7 @@
         <?php endif ?>
         
         <?php if($_SESSION["versionFormatError"]) : ?>
-            <p class="errorMessage">バージョンの形式は「[数字2桁].[数字2桁].[数字2桁]」となるようにしてください</p>
+            <p class="errorMessage">バージョンの形式は「[数字1~2桁].[数字1~2桁].[数字1~2桁]」となるようにしてください</p>
         <?php endif ?>
         
         <?php if($_SESSION["versionEmptyError"]) : ?>
