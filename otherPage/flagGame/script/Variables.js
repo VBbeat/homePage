3fc691698b7b34fc28a89e4195aebdfc88ed5d68
kouheyHEY <vudtarm@gmail.com>
2022-01-03 00:38:06 +0900

    update flagGame

---
+++ otherPage/flagGame/script/Variables.js
@@ -0,0 +1,14 @@
+// 旗の状態を表す定数
+const FLAG_STATE_UP = 1;
+const FLAG_STATE_DOWN = 0;
+
+// 旗の色を表す定数
+const FLAG_COLOR_RED = "#FF4444";
+const FLAG_COLOR_WHITE = "#444444";
+
+// 旗の状態
+var flag_state_white;
+var flag_state_red;
+
+// 指示をクリアした回数
+var flag_clear_num;
---
+++ otherPage/flagGame/script/flagGame.js
@@ -0,0 +1,8 @@
+function init() {
+    // 旗の状態の初期化
+    flag_state_white = FLAG_STATE_DOWN;
+    flag_state_red = FLAG_STATE_DOWN;
+
+    // 指示クリア回数の初期化
+    flag_clear_num = 0;
+}
