652184fade1e3796f5ee34e536bb0a787ba9942e
kouheyHEY <vudtarm@gmail.com>
2022-01-05 00:09:37 +0900

    define variable

--- otherPage/flagGame/script/Variables.js
+++ otherPage/flagGame/script/Variables.js
@@ -6,9 +6,19 @@ const FLAG_STATE_DOWN = 0;
 const FLAG_COLOR_RED = "#FF4444";
 const FLAG_COLOR_WHITE = "#444444";
 
+// 指示文
+const ORDERTEXT_INIT = "ここに指示文が表示されます";
+
 // 旗の状態
 var flag_state_white;
 var flag_state_red;
 
 // 指示をクリアした回数
 var flag_clear_num;
+
+// 指示文（色）
+// 指示文（状態）
+// 指示文（全体）
+var orderText_color;
+var orderText_state;
+var orderText_all;
--- otherPage/flagGame/script/flagGame.js
+++ otherPage/flagGame/script/flagGame.js
@@ -1,3 +1,6 @@
+/**
+ * 変数を初期化する
+ */
 function init() {
     // 旗の状態の初期化
     flag_state_white = FLAG_STATE_DOWN;
@@ -5,4 +8,7 @@ function init() {
 
     // 指示クリア回数の初期化
     flag_clear_num = 0;
+
+    // 指示文のリセット
+    orderText_all = ORDERTEXT_INIT;
 }
