43af7d989122af113703416d675839ec92dc5b8b
kouheyHEY <vudtarm@gmail.com>
2022-01-27 21:00:31 +0900

    update js

--- otherPage/flagGame/script/Variables.js
+++ otherPage/flagGame/script/Variables.js
@@ -2,16 +2,22 @@
 const FLAG_STATE_UP = 1;
 const FLAG_STATE_DOWN = 0;
 
+// 旗の色のコードを表す定数
+const FLAG_COLOR_CODE_RED = "#FF4444";
+const FLAG_COLOR_CODE_WHITE = "#444444";
+
 // 旗の色を表す定数
-const FLAG_COLOR_RED = "#FF4444";
-const FLAG_COLOR_WHITE = "#444444";
+const FLAG_COLOR_RED = "red";
+const FLAG_COLOR_WHITE = "white";
 
 // 指示文
 const ORDERTEXT_INIT = "ここに指示文が表示されます";
 
 // 旗の状態
-var flag_state_white;
-var flag_state_red;
+var flag_state = {
+    white: FLAG_STATE_DOWN,
+    red: FLAG_STATE_UP
+};
 
 // 指示をクリアした回数
 var flag_clear_num;
--- otherPage/flagGame/script/flagGame.js
+++ otherPage/flagGame/script/flagGame.js
@@ -12,3 +12,15 @@ function init() {
     // 指示文のリセット
     orderText_all = ORDERTEXT_INIT;
 }
+
+/**
+ * 旗の状態の切り替え
+ * @param flagColor 旗の色
+ * @param flagPosition 旗の上下
+ */
+function changeFlagState(flagColor, flagPosition) {
+    // 旗の位置を変える
+    flag_state[flagColor] = flagPosition;
+
+    // 旗の位置
+}
