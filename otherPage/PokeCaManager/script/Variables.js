f355b094cdda442655e9e9c332007ff5c07cee0f
kouheyHEY <vudtarm@gmail.com>
2022-02-03 01:36:48 +0900

    update var

---
+++ otherPage/PokeCaManager/script/Variables.js
@@ -0,0 +1,19 @@
+// カードの種類を表す定数
+const CARD_POKEMON = "0";
+const CARD_SUPPORT = "1";
+const CARD_GOODS = "2";
+const CARD_STADIUM = "3";
+const CARD_ENERGY = "4";
+
+// デッキの構成を表す
+const CARD_NAME = "0";
+const CARD_NUM = "1";
+
+// デッキの配列
+var deckCardArray = [
+    [],
+    [],
+    [],
+    [],
+    []
+];
