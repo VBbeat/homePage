8ec1b99488a2b1fff32c25ccaed48492ecbf3694
kouheyHEY <vudtarm@gmail.com>
2022-04-13 23:48:03 +0900

    set class

---
+++ otherPage/rabbitShooting/script/variable.js
@@ -0,0 +1,12 @@
+// アセット
+var ASSETS = {
+    image: {
+        player_normal: './img/chara/player/player_normal.png',
+        player_power: './img/chara/player/player_power.png',
+        player_range: './img/chara/player/player_range.png',
+        player_speed: './img/chara/player/player_speed.png',
+        player_super: './img/chara/player/player_super.png',
+
+        boss_1: './img/chara/boss_1.png'
+    }
+};
