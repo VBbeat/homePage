caa01126ce2bc6d6092cfe420e06ba430dbf4ce0
kouheyHEY <vudtarm@gmail.com>
2022-04-19 00:25:40 +0900

    update

--- otherPage/rabbitShooting/script/main.js
+++ otherPage/rabbitShooting/script/main.js
@@ -5,7 +5,24 @@ phina.globalize();
 phina.main(function () {
     // アプリケーション生成
     var app = GameApp({
-        assets: ASSETS,
+        assets: {
+            image: {
+                player_normal: './img/chara/player/player_normal.png',
+                player_power: './img/chara/player/player_power.png',
+                player_range: './img/chara/player/player_range.png',
+                player_speed: './img/chara/player/player_speed.png',
+                player_super: './img/chara/player/player_super.png',
+
+                boss_1: './img/chara/boss_1.png',
+
+                bg_title: './img/bg/title.png',
+
+                bullet_normal: './img/bullet/player/bullet_normal.png',
+                bullet_speed: './img/bullet/player/bullet_speed.png',
+            },
+
+            font: { 'bitFont': './font/PixelMplus12-Regular.ttf' },
+        },
         scenes: SCENES,
         startLabel: 'title',
         fps: 60,
