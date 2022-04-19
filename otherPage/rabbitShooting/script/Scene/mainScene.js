fde74139154b85ecbd3403700f9dfd4c006f9a42
kouheyHEY <vudtarm@gmail.com>
2022-04-20 01:01:46 +0900

    add method collision

--- otherPage/rabbitShooting/script/Scene/mainScene.js
+++ otherPage/rabbitShooting/script/Scene/mainScene.js
@@ -28,9 +28,40 @@ phina.define('GameScene', {
     onpointstay: function (e) {
 
         if (this.currentFrame % INTERVAL_BULLET_NORMAL == 0) {
+            // 発射間隔を空けたのち、発射処理を実行
+
             // プレイヤーの弾を生成
             Bullet_normal().addChildTo(this.playerBulletGroup).setPosition(this.player.x, this.player.y);
         }
+    },
+
+    // 敵機当たり判定
+    hitTestPlayer: function () {
+        // thisを退避
+        var self = this;
+
+        // [繰返]プレイヤーが発射した弾に対する処理
+        self.playerBulletGroup.children.each(function (bullet) {
+            // TODO: ここの処理を書く
+            // 円判定
+            var a = Circle(self.enemy.x, self.enemy.y, 20);
+            var b = Circle(bullet.x, bullet.y, 10);
+
+
+            if (Collision.testCircleCircle(a, b)) {
+
+                --PLAYER_HP;
+                if (PLAYER_HP > 0) {
+                    self.Impact(bullet.x, bullet.y);
+                    bullet.remove();
+                } else {
+                    self.exit('result', {
+                        score: 0,
+                        message: 'Game Over'
+                    });
+                }
+            }
+        });
     },
 
     // 毎フレーム更新処理
